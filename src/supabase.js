1cbda210a2a7113ab332bfe199eec456bf93161f
Manish614 <manishvedwal007@gmail.com>
2025-08-05 23:13:04 +0530

    envv

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ebkrbkabsdshvlelzclx.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVia3Jia2Fic2RzaHZsZWx6Y2x4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxNjE3NzIsImV4cCI6MjA2OTczNzc3Mn0.X6ikfDvaedNcAdpdLquNAkamciuT_8y7ZjSQuvLuvwQ";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
